df5fcbb5e02ec17053d35b841ae274027f3bf917
codewithegil <egilnzulu@gmail.com>
2026-07-19 03:58:46 +0000

    chore: update remaining services with eureka client configurations

--- backend/services/application-service/src/main/java/com/codequest/applicationservice/ApplicationServiceApplication.java
+++ backend/services/application-service/src/main/java/com/codequest/applicationservice/ApplicationServiceApplication.java
@@ -1,6 +1,5 @@
 package com.codequest.applicationservice;
 
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- backend/services/discovery-service/src/main/java/com/codequest/discoveryservice/DiscoveryServiceApplication.java
+++ backend/services/discovery-service/src/main/java/com/codequest/discoveryservice/DiscoveryServiceApplication.java
@@ -8,6 +8,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer // <-- This annotation does all the heavy lifting!
 public class DiscoveryServiceApplication {
     public static void main(String[] args) {
+
+        System.setProperty("server.port", "8761");
+        System.setProperty("eureka.client.register-with-eureka", "false");
+        System.setProperty("eureka.client.fetch-registry", "false");
         SpringApplication.run(DiscoveryServiceApplication.class, args);
     }
 }
